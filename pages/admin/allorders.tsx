224fc7276104147e690b93d7d621811d49d21ea4
nilesh-singh-2304 <shyaam2304@gmail.com>
2024-07-02 20:56:41 +0530

    updated admin

--- pages/admin/allorders.tsx
+++ pages/admin/allorders.tsx
@@ -137,7 +137,7 @@ const Allorders = () => {
         <tbody>
             <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
+                    Apple MacBook Pro 17
                 </th>
                 <td className="px-6 py-4">
                     Silver
